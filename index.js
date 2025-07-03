1e30019a30d4f9ae75bfea6373a120ef1d27905c
Lakshetaa R <lakshetaa205@gmail.com>
2025-07-03 21:40:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const newGame=document.getElementById("roll");
+const roll=document.getElementById("rollDice");
+const hold=document.getElementById("hold");
+const dice=document.getElementById("dice");
+const currentScore0=document.getElementById("current--0");
+const currentScore1=document.getElementById("current--1");
+const tScore0=document.getElementById("score--0");
+const tScore1=document.getElementById("score--1");
+const winner=document.getElementById("winner");
+let activePlayer=0;
+let tScores0=0;
+let tScores1=0;
+let cScore0=0;
+let cScore1=0;
+
+const rollDice = () =>{
+    dice.style.display = "none";
+    let random=Math.trunc(Math.random()*6)+1;
+    dice.src = `${random}.jpg`;
+    dice.style.display = "block";
+    
+    if(random !== 1){
+        if(tScores0 >= 100){
+            winner.textContent = "Player 1 wins!";
+            tScore0.textContent = tScores0;
+            setTimeout(() => {
+                resetGame();
+            }, 2000);
+            return;
+        }else if(tScores1>=100){
+            winner.textContent = "Player 2 wins!";
+            tScore1.textContent = tScores1;
+            setTimeout(() => {
+                resetGame();
+            }, 2000);
+            return;
+        }
+        if(activePlayer === 0){
+            cScore0 += random;
+            tScores0 += random;
+            currentScore0.textContent = cScore0;
+        } else {
+            cScore1 += random;
+            tScores1 += random;
+            currentScore1.textContent = cScore1;
+        }
+    }else{
+        switchPlayer();
+    }
+};
+
+const switchPlayer = () => {
+    if(activePlayer === 0){
+        activePlayer=1;
+        cScore0 = 0;
+        currentScore0.textContent = cScore0;
+        tScore0.textContent = tScores0;
+    }else{
+        activePlayer=0;
+        cScore1=0;
+        currentScore1.textContent = cScore1;
+        tScore1.textContent = tScores1;
+    }
+};
+
+const resetGame = () => {
+    dice.style.display = "none";
+    activePlayer=0;
+    tScores0=0;
+    tScores1=0;
+    cScore0=0;
+    cScore1=0;
+    currentScore0.textContent = cScore0;
+    currentScore1.textContent = cScore1;
+    tScore0.textContent = tScores0;
+    tScore1.textContent = tScores1;
+    winner.textContent = "";
+}
+roll.addEventListener("click", rollDice);
+hold.addEventListener("click", switchPlayer);
+newGame.addEventListener("click", resetGame);
